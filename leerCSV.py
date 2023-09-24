fdf8cd61a230786579ea13f5529c99f1dbc798d7
Fidel <fidel.morales.briones@gmail.com>
2023-09-23 22:11:33 -0600

    Avance GPT, ya se lee el CSV y regresa un análisis

---
+++ leerCSV.py
@@ -0,0 +1,116 @@
+import os
+import csv
+from softtek_llm.chatbot import Chatbot
+from softtek_llm.models import OpenAI
+from softtek_llm.cache import Cache
+from softtek_llm.vectorStores import PineconeVectorStore
+from softtek_llm.embeddings import OpenAIEmbeddings
+from softtek_llm.schemas import Filter
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if OPENAI_API_KEY is None:
+    raise ValueError("OPENAI_API_KEY not found in .env file")
+
+OPENAI_API_BASE = os.getenv("OPENAI_API_BASE")
+if OPENAI_API_BASE is None:
+    raise ValueError("OPENAI_API_BASE not found in .env file")
+
+OPENAI_EMBEDDINGS_MODEL_NAME = os.getenv("OPENAI_EMBEDDINGS_MODEL_NAME")
+if OPENAI_EMBEDDINGS_MODEL_NAME is None:
+    raise ValueError("OPENAI_EMBEDDINGS_MODEL_NAME not found in .env file")
+
+OPENAI_CHAT_MODEL_NAME = os.getenv("OPENAI_CHAT_MODEL_NAME")
+if OPENAI_CHAT_MODEL_NAME is None:
+    raise ValueError("OPENAI_CHAT_MODEL_NAME not found in .env file")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+if PINECONE_API_KEY is None:
+    raise ValueError("PINECONE_API_KEY not found in .env file")
+
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+if PINECONE_ENVIRONMENT is None:
+    raise ValueError("PINECONE_ENVIRONMENT not found in .env file")
+
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+if PINECONE_INDEX_NAME is None:
+    raise ValueError("PINECONE_INDEX_NAME not found in .env file")
+
+vector_store = PineconeVectorStore(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENVIRONMENT,
+    index_name=PINECONE_INDEX_NAME,
+)
+
+embeddings_model = OpenAIEmbeddings(
+    api_key=OPENAI_API_KEY,
+    model_name=OPENAI_EMBEDDINGS_MODEL_NAME,
+    api_type="azure",
+    api_base=OPENAI_API_BASE,
+)
+
+cache = Cache(
+    vector_store=vector_store,
+    embeddings_model=embeddings_model,
+)
+
+model = OpenAI(
+    api_key=OPENAI_API_KEY,
+    model_name=OPENAI_CHAT_MODEL_NAME,
+    api_type="azure",
+    api_base=OPENAI_API_BASE,
+    verbose=False,
+)
+
+filters = [
+    Filter(
+        type="DENY",
+        case="ANYTHING about the Titanic. YOU CANNOT talk about the Titanic AT ALL.",
+    )
+]
+
+chatbot = Chatbot(
+    model=model,
+    description="You are a very helpful and polite chatbot",
+    filters=filters,
+    cache=cache,
+    verbose=False,
+)
+
+#método para generar una prompt y regresar la respuesta
+#input: prompt en formato string
+#output: respuesta de gpt en formato string
+def get_answer(inputPrompt):
+    while True:
+        try:
+            response = chatbot.chat(
+                prompt= inputPrompt,
+                print_cache_score=False,
+            )
+            break
+        except:
+            pass
+    respuesta = response.message.content
+
+    return respuesta
+
+#método para leer un csv, y hacer un análisis con gpt
+#input: un archivo csv
+#output: prompt para gpt
+def generate_prompt_from_csv(file_path):
+    # Read the CSV file
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        data = list(csv_reader)
+
+    # Generate a prompt based on the CSV data
+    prompt = f"Here is a CSV file:\n\n"
+    
+    for row in data[:]:  # Display the first 5 rows as an example
+        prompt += ', '.join(row) + '\n'
+    
+    prompt += f"Obtain the min, max, mean, and range of each column and based on that information give a general analysis of the dataset, and in a new paragraph give quick tips to reduce the emitions of lead, nitrox"
+    return prompt
+
+
+print(get_answer(generate_prompt_from_csv("contaminantes.csv")))
