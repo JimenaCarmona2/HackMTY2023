a37af48941f007880a7a6c0ccd850498bdadf5c2
JimenaCarmona1 <a00835729@tec.mx>
2023-09-23 19:11:46 -0600

    Merge branch 'main' of https://github.com/JimenaCarmona1/HackMTY2023

---
+++ src/app/pages/prompt/page.js
@@ -0,0 +1,54 @@
+"use client"
+
+// Importa React y useState
+import React, { useState } from 'react';
+
+// Importa los estilos CSS desde './page.module.css'
+import styles from './page.module.css';
+
+export default function Home() {
+  // Inicializa el estado para el valor del campo de entrada y el valor enviado
+  const [inputValue, setInputValue] = useState('');
+  const [submittedValue, setSubmittedValue] = useState('');
+
+  // Maneja los cambios en el campo de entrada
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Maneja el envío del formulario
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedValue(inputValue);
+  };
+
+  return (
+    // Utiliza las clases de estilo definidas en 'page.module.css'
+    <div className={styles.container}>
+      <h1 className={styles.title}>Sustainability Level Analyzer Software</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.label}>
+          Escriba una instrucción o pregunta:
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            required
+            className={styles.input}
+          />
+        </label>
+        <button type="submit" className={styles.button}>
+          Enviar
+        </button>
+      </form>
+      {submittedValue && (
+        <div className={styles.result}>
+          <h2>Texto ingresado:</h2>
+          <p>{submittedValue}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
